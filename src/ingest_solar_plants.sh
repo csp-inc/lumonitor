a89e4acfb764c5bb842a7e9bf7f2246f4030a0ef
Jesse Anderson <jesse@csp-inc.org>
2020-10-28 11:20:16 -0700

    Getting underway before we're underwater

--- src/ingest_solar_plants.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-ogr2ogr -dialect sqlite -sql "SELECT * FROM PowerPlants_US_202004 WHERE PrimSource = 'solar'" data/solar_points.shp /vsizip/vsicurl/https://www.eia.gov/maps/map_data/PowerPlants_US_EIA.zip
-
-gsutil cp data/solar_points.* gs://aft-saf
-
-earthengine upload table --wait --asset_id users/jesse/lumonitor/solar_points gs://aft-saf/solar_points.shp
--- src/solar_constants.py
+++
@@ -1,9 +0,0 @@
-import ee
-ee.Initialize()
-
-L8SR = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
-# Use these bands for prediction.
-BANDS = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7']
-FEATURE_NAMES = list(BANDS)
-FEATURE_NAMES.append('solar')
-N_CLASSES = 2
