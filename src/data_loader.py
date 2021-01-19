04c17739b4027cc60c717b327d2114d805c924d4
Jesse Anderson <jesse@csp-inc.org>
2021-01-19 10:33:44 -0700

    working version of hm model

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,33 +4,30 @@ def parse_tfrecord(example_proto, features_dict):
     return tf.io.parse_single_example(example_proto, features_dict)
 
 def to_tuple(inputs, features, predictors, label):
-    inputs_list = [ inputs.get(key) for key in features ]
+    inputs_list = [inputs.get(key) for key in features]
     inputs_stack = tf.stack(inputs_list, axis=0)
-    transposed_stack = tf.transpose(inputs_stack, [1,2,0])
-    return (transposed_stack[:,:,:len(predictors)], 
-            transposed_stack[:,:,features.index(label)])
+    transposed_stack = tf.transpose(inputs_stack, [1, 2, 0])
+    return (transposed_stack[:, :, :len(predictors)],
+            transposed_stack[:, :, features.index(label)])
 
 
 def load_data(path, predictors, label, train_size=None, batch_size=None):
-    kernel_shape = [256, 256] 
+    kernel_shape = [256, 256]
     feature = tf.io.FixedLenFeature(
-            shape=kernel_shape, 
-            dtype=tf.float32, 
-    # There are some missing records, for now just fill w/ 0
-            default_value=tf.fill(kernel_shape, 0.0))
-    features = predictors + [label] 
+        shape=kernel_shape,
+        dtype=tf.float32,
+        # There are some missing records, for now just fill w/ 0
+        default_value=tf.fill(kernel_shape, 0.0))
+    features = predictors + [label]
     columns = [feature for n in features]
     features_dict = dict(zip(features, columns))
 
     glob = tf.io.gfile.glob(path)
-    d = (tf.data.TFRecordDataset(glob, compression_type='GZIP') 
-            .map(lambda x: parse_tfrecord(x, features_dict))
-            .map(lambda x: to_tuple(x, features, predictors, label)))
+    d = (tf.data.TFRecordDataset(glob, compression_type='GZIP')
+         .map(lambda x: parse_tfrecord(x, features_dict))
+         .map(lambda x: to_tuple(x, features, predictors, label)))
 
     if train_size is not None:
         d = d.shuffle(train_size)
 
-    if batch_size is not None:
-        d = d.batch(batch_size)
-
-    return d
+    return d.batch(batch_size).repeat()
--- src/run_model.py
+++ src/run_model.py
@@ -6,11 +6,11 @@ import data_loader
 import model_runner
 
 predictor_names = ['B1','B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B10', 'B11']
-label_name = 'imp'
+label_name = 'hm'
 # DRY this
 feature_names = predictor_names + [ label_name ]
 
-data_dir = './gcs_to_az_tmp'
+data_dir = './data'
 training_data_path = os.path.join(data_dir, 'sample_data_??.tfrecord.gz')
 
 train_size = 2500
@@ -28,7 +28,8 @@ testing_data_path = os.path.join(data_dir, 'sample_data_?.tfrecord.gz')
 testing_data = data_loader.load_data(
         path=testing_data_path,
         predictors=predictor_names,
-        label=label_name
+        label=label_name,
+        batch_size=1
         )
 
 optimizer = 'SGD'
@@ -42,12 +43,12 @@ model = model_runner.get_model(
         metric_names=metrics
         )
 
-epochs = 10
+epochs = 50
 eval_size = 8000
 
 model.fit(
-    x=training_data, 
-    epochs=epochs, 
-    steps_per_epoch=int(train_size / batch_size), 
+    x=training_data,
+    epochs=epochs,
+    steps_per_epoch=int(train_size / batch_size),
     validation_data=testing_data,
     validation_steps=eval_size)
