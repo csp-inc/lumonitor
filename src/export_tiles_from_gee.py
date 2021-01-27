975f64cdbc4352bde7616adf33f3cea119d8c49b
Jesse Anderson <jesse@csp-inc.org>
2021-01-27 15:43:20 -0700

    Addded gee tile exports

---
+++ src/export_tiles_from_gee.py
@@ -0,0 +1,48 @@
+import os
+
+import ee
+ee.Initialize()
+
+from HLSTileLookup import HLSTileLookup
+
+def export_tile_for_ee_image(ee_image, xmin, ymax, epsg, tile_id, prefix):
+    res = 30
+    cells = 3600
+    diff = res * cells
+    xmax = xmin + diff
+    ymin = ymax - diff
+    epsg_string = 'EPSG:' + str(epsg)
+    projection = ee.Projection(epsg_string)
+    tile = ee.Geometry.Rectangle([xmin, ymin, xmax, ymax], projection, False)
+
+    task = ee.batch.Export.image.toCloudStorage(
+        image=ee_image,
+        bucket='lumonitor',
+        fileNamePrefix=os.path.join('hls_tiles', prefix + '_' + tile_id),
+        dimensions=cells,
+        region=tile,
+        crs=epsg_string
+        )
+    task.start()
+
+
+def export_tiles_for_ee_image(ee_image, bbox):
+    tile_lookup = HLSTileLookup()
+    tile_df = tile_lookup.get_bbox_hls_tile_info(*bbox)
+    for _, row in tile_df.iterrows():
+        export_tile_for_ee_image(
+            ee_image=ee_image,
+            xmin=row['Xstart'],
+            ymax=row['Ystart'],
+            epsg=row['EPSG'],
+            tile_id=row['TilID'],
+            prefix='hm'
+        )
+
+
+hm = ee.Image('projects/GEE_CSP/HM/HM_ee_2017_v014_500_30')
+nlcd_imp = ee.Image('USGS/NLCD/NLCD2016').select('impervious').divide(100).float()
+hm = hm.addBands(nlcd_imp)
+
+bbox = [-124.9, 24.4, -66.7, 49.5]
+export_tiles_for_ee_image(hm, bbox)
