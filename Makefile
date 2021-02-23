ad1c8175bb7b6c227ff08654f9a46a2d24aab28c
Jesse Anderson <jesse@csp-inc.org>
2021-02-23 14:53:09 -0700

    Refactored Unet.py

--- Makefile
+++ Makefile
@@ -4,22 +4,38 @@ ifneq (,$(wildcard ./.env))
     export
 endif
 
-VPATH=src/:data/
+DATA=data/
+BLOB_DIR=$(DATA)blobs
+COG_DIR=$(DATA)cog
+
+VPATH=src/:$(DATA):$(BLOB_DIR):$(COG_DIR)
 SHELL=/usr/bin/env bash
 
 BLOBS=$(shell az storage blob list --delimiter 'zarr' -c hls --account-key=$(AZURE_EASTUS2_STORAGE_KEY) --account-name=lumonitoreastus2 -o tsv | tr '\n' ' ')
+$(INFO $(BLOBS))
 
-BLOB_PLACEHOLDERS=$(patsubst %,data/cog/%.blob, $(BLOBS))
-COGS=$(patsubst %.zarr.blob,%.tif, $(BLOB_PLACEHOLDERS))
+#GCS=$(shell gsutil ls gs://lumonitor/hls_tiles/)
+
+#GCS_PLACEHOLDERS=$(patsubst %,data/blobs/%.blob, $(notdir $(GCS)))
+#LABEL_COGS=$(patsubst %.blob,%, $(GCS_PLACEHOLDERS))
+
+BLOB_PLACEHOLDERS=$(patsubst %,data/blobs/%.blob, $(BLOBS))
+FEATURE_COGS=$(patsubst data/blobs/%.zarr.blob,data/cog/%.tif, $(BLOB_PLACEHOLDERS))
+$(info $(FEATURE_COGS))
 
 .PHONY: all
-all: $(COGS)
+all: $(FEATURE_COGS)
 
 $(BLOB_PLACEHOLDERS): %:
-$(COGS): %.tif: | %.zarr.blob
+$(FEATURE_COGS): %.tif: | %.zarr.blob
 
-data/cog/%.tif: data/cog/%.zarr.blob
+data/cog/%.tif: data/blobs/%.tif.gcs
+
+data/cog/%.tif: data/blobs/%.zarr.blob
 	python3 src/utils/zarr_to_cog.py az://hls/$*.zarr $@ --account-name=$(AZURE_EASTUS2_STORAGE_ACCOUNT) --account-key=$(AZURE_EASTUS2_STORAGE_KEY)
+
+data/%.gcs:
+	touch $@
 
 data/%.blob: 
 	touch $@
--- src/Unet.py
+++ src/Unet.py
@@ -11,45 +11,67 @@ def conv_block(in_channels, out_channels):
         nn.ReLU(inplace=True)
     )
 
-def encoder_block(in_channels, out_channels, x):
-    encoder = conv_block(in_channels, out_channels)(x)
-    pooled_encoder = nn.MaxPool2d(2)(encoder)
-    return pooled_encoder, encoder
-
-def decoder_block(in_channels, out_channels, x, concatter):
-    decoder = nn.ConvTranspose2d(
+def decoder_block(in_channels, out_channels):
+    return nn.ConvTranspose2d(
         in_channels,
         out_channels,
         kernel_size=(2, 2),
         stride=(2, 2),
         padding=0
-    )(x)
-    decoder = torch.cat((decoder, concatter), dim=1)
-    decoder = nn.BatchNorm2d(in_channels)(decoder)
-    decoder = nn.ReLU(inplace=True)(decoder)
-    return conv_block(in_channels, out_channels)(decoder)
+    )
+
+def norm_relu(in_channels, out_channels):
+    return nn.Sequential(
+        nn.BatchNorm2d(in_channels),
+        nn.ReLU(inplace=True),
+        conv_block(in_channels, out_channels)
+    )
 
 class Unet(torch.nn.Module):
     def __init__(self, in_channels):
         super(Unet, self).__init__()
         self.in_channels = in_channels
-
-    def forward(self, x):
-        pooled_encoder0, encoder0 = encoder_block(self.in_channels, 32, x)
-        pooled_encoder1, encoder1 = encoder_block(32, 64, pooled_encoder0)
-        pooled_encoder2, encoder2 = encoder_block(64, 128, pooled_encoder1)
-        pooled_encoder3, encoder3 = encoder_block(128, 256, pooled_encoder2)
-        pooled_encoder4, encoder4 = encoder_block(256, 512, pooled_encoder3)
-        center = conv_block(512, 1024)(pooled_encoder4)
-        decoder4 = decoder_block(1024, 512, center, encoder4)
-        decoder3 = decoder_block(512, 256, decoder4, encoder3)
-        decoder2 = decoder_block(256, 128, decoder3, encoder2)
-        decoder1 = decoder_block(128, 64, decoder2, encoder1)
-        decoder0 = decoder_block(64, 32, decoder1, encoder0)
-
-        output = nn.Sequential(
+        self.encoder_block_0 = conv_block(self.in_channels, 32)
+        self.encoder_block_1 = conv_block(32, 64)
+        self.encoder_block_2 = conv_block(64, 128)
+        self.encoder_block_3 = conv_block(128, 256)
+        self.encoder_block_4 = conv_block(256, 512)
+        self.center = conv_block(512, 1024)
+        self.decoder_block_4 = decoder_block(1024, 512)
+        self.post_decoder_4 = norm_relu(1024, 512)
+        self.decoder_block_3 = decoder_block(512, 256)
+        self.post_decoder_3 = norm_relu(512, 256)
+        self.decoder_block_2 = decoder_block(256, 128)
+        self.post_decoder_2 = norm_relu(256, 128)
+        self.decoder_block_1 = decoder_block(128, 64)
+        self.post_decoder_1 = norm_relu(128, 64)
+        self.decoder_block_0 = decoder_block(64, 32)
+        self.post_decoder_0 = norm_relu(64, 32)
+        self.output = nn.Sequential(
             nn.Conv2d(32, 1, (1, 1)),
             nn.Sigmoid()
-        )(decoder0)
+        )
 
-        return output
+    def forward(self, x):
+        encoder_0 = self.encoder_block_0(x)
+        x = nn.MaxPool2d(2)(encoder_0)
+        encoder_1 = self.encoder_block_1(x)
+        x = nn.MaxPool2d(2)(encoder_1)
+        encoder_2 = self.encoder_block_2(x)
+        x = nn.MaxPool2d(2)(encoder_2)
+        encoder_3 = self.encoder_block_3(x)
+        x = nn.MaxPool2d(2)(encoder_3)
+        encoder_4 = self.encoder_block_2(x)
+        x = nn.MaxPool2d(2)(encoder_4)
+        x = self.center(x)
+        decoder_4 = self.decoder_block_4(x)
+        x = self.post_decoder_4(torch.cat(decoder_4, encoder_4))
+        decoder_3 = self.decoder_block_3(x)
+        x = self.post_decoder_3(torch.cat(decoder_3, encoder_3))
+        decoder_2 = self.decoder_block_2(x)
+        x = self.post_decoder_2(torch.cat(decoder_2, encoder_2))
+        decoder_1 = self.decoder_block_1(x)
+        x = self.post_decoder_1(torch.cat(decoder_1, encoder_1))
+        decoder_0 = self.decoder_block_0(x)
+        x = self.post_decoder_0(torch.cat(decoder_0, encoder_0))
+        return self.output(x)
